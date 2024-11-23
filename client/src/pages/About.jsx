37250ed0dce1854c21f79500ad4953829f86bade
ShivangiT25 <shivangit2596@gmail.com>
2024-11-23 18:37:41 +0530

    Initial Setup Completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,20 +4,21 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About Social Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Welcome to Social Blog! This blog was created by Shivangi Thakur
+              as a personal project to share one's thoughts and ideas with the
+              world. As a passionate developer who loves to write about
+              technology, coding, and everything in between, must share 
+              his/her thoughts with world too.
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
+              languages. If you want to always learn and explore new
               technologies, so be sure to check back often for new content!
             </p>
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -48,8 +48,8 @@ export default function SignIn() {
         {/* left */}
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+            <span className='px-2 py-1 bg-gradient-to-r from-violet-500 via-pink-500 to-purple-900 rounded-xl text-white'>
+              Social
             </span>
             Blog
           </Link>
